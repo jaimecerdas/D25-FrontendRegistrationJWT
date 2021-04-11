ea6bd06902b32b0e74f7b8188b0a03f147f6cdab
J Cerdas <jaimecerdas@gmail.com>
2021-04-11 01:57:38 +0000

    copy of Frontend

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+import { Link } from "react-router-dom";
+
+export const Home = () => {
+	return (
+		<div className="text-center mt-5">
+			<h1>Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<Link to="/register" className="btn btn-success">
+				Register!
+			</Link>
+			<Link to="/login" className="btn btn-primary">
+				Log In
+			</Link>
+		</div>
+	);
+};
